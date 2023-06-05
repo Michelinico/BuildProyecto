39b3de90e86575f83b6230b806abc7f860d70932
Michelinico <116155476+Michelinico@users.noreply.github.com>
2023-06-05 09:16:30 +0200

    Delete build directory

--- build/Modelo/Cabeceras.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Credentials: true');
-header("Access-Control-Allow-Headers: content-type");
-header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
-?>
