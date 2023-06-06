5b35bd84f69307db5afadd1809af9edc3ab5023e
Michelinico <116155476+Michelinico@users.noreply.github.com>
2023-06-06 11:02:34 +0200

    Cambios en las consultas

--- build/Modelo/ParkingDB.php
+++ build/Modelo/ParkingDB.php
@@ -19,45 +19,33 @@ class ParkingDB {
         $this->usuario=$credenciales["usuario"];
         $this->clave=$credenciales["clave"];
         $this->basedatos=$credenciales["basedatos"];
-
-        /*$this->conexion = new mysqli($this->servidor, $this->usuario, $this->clave, $this->basedatos);
+	    
+	$this->conexion = mysqli_connect($this->servidor, $this->usuario, $this->clave, $this->basedatos);
         if ($this->conexion->connect_error == true) {
             die("Error de conexion".$this->conexion->connect_error);
-        }*/
-        
-        try {
-         $this->conexion = new PDO("mysql:host=$this->servidor;dbname=$this->basedatos", $this->usuario, $this->clave);
-            // Establecer el modo de error de PDO a excepciones
-            $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-            die("Error de conexión: " . $e->getMessage());
         }
     }
 
     // USUARIOS //
 
     public function comprueba($usuario) {
+
         $exito=false;
         $login=$usuario['DNI'];
         $clave=$usuario['clave'];
-	$sql = "SELECT Nombre, TipoUsu FROM usuarios WHERE DNI = :login AND Contrasena = :clave";
-    $consulta = $this->conexion->prepare($sql);
-    $consulta->bindParam(':login', $login);
-    $consulta->bindParam(':clave', $clave);
-    $consulta->execute();
-
-    $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
-
-    if ($resultado !== false) {
-        $exito = true;
-        $nombreUsuario = $resultado["Nombre"];
-        $tipoUsuario = $resultado["TipoUsu"];
-        $datosUsuario = array(
-            "DNI" => $login,
-            "nombre" => $nombreUsuario,
-            "tipo" => $tipoUsuario
-        );
-    }
+        $sql = "select Nombre,TipoUsu from usuarios where DNI = '$login' and Contrasena = '$clave'";
+        $resultado=$this->conexion->query($sql);
+        if ($resultado->num_rows > 0) {
+            $exito = true;
+            $fila = $resultado->fetch_assoc();
+            $nombreUsuario = $fila["Nombre"];
+            $tipoUsuario = $fila["TipoUsu"];
+            $datosUsuario = array(
+                "DNI" => $login,
+                "nombre" => $nombreUsuario,
+                "tipo" => $tipoUsuario
+            );
+        }
         return $exito ? $datosUsuario : null;
     }
 
@@ -80,15 +68,13 @@ class ParkingDB {
             )
             GROUP BY es1.Matricula
             ORDER BY es1.Matricula;";
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->execute();
-        $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($resultados as $fila) {
-            $estado = new Estado($fila);
+        $cursor = $this->conexion->query($sql);
+        $tupla = $cursor->fetch_assoc();  
+        while ($tupla != null) {
+            $estado = new Estado($tupla);
             array_push($estados, $estado->toArray());
+            $tupla = $cursor->fetch_assoc();
         }
-
         return $estados;
     }
 
@@ -96,21 +82,16 @@ class ParkingDB {
 
     public function cochesUsu($usuario) {
         $coches = array();
-
-        $sql = "SELECT * FROM coches WHERE DNI = :usuario";
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':usuario', $usuario);
-        $consulta->execute();
-
-        $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($resultados as $fila) {
-            $coche = new Coche($fila);
+        $sql="select * from coches where DNI='$usuario'";
+        $cursor=$this->conexion->query($sql);
+        $tupla = $cursor->fetch_assoc();
+        while ($tupla != null) {
+            $coche = new Coche($tupla);
             array_push($coches, $coche->toArray());
+            $tupla = $cursor->fetch_assoc();
         }
-
         return $coches;
-}
+    }
 
     // VEHICULOS ADMIN //
 
@@ -125,105 +106,73 @@ class ParkingDB {
 
     public function leerCoches() {
         $coches = array();
-
-        $sql = "SELECT * FROM coches";
-        $consulta = $this->conexion->query($sql);
-
-        while ($tupla = $consulta->fetch(PDO::FETCH_ASSOC)) {
+        $sql="select * from coches";
+        $cursor=$this->conexion->query($sql);
+        $tupla = $cursor->fetch_assoc();
+        while ($tupla != null) {
             $coche = new Coche($tupla);
             array_push($coches, $coche);
+            $tupla = $cursor->fetch_assoc();
         }
-
         return $coches;
     }
 
     public function cocheUsu($matricula) {
         $coches = array();
-
-        $sql = "SELECT * FROM coches WHERE Matricula = :matricula";
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':matricula', $matricula);
-        $consulta->execute();
-
-        while ($tupla = $consulta->fetch(PDO::FETCH_ASSOC)) {
+        $sql="select * from coches where Matricula='$matricula'";
+        $cursor=$this->conexion->query($sql);
+        $coches = $cursor->fetch_assoc();
+        while ($tupla != null) {
             $coche = new Coche($tupla);
             array_push($coches, $coche);
+            $tupla = $cursor->fetch_assoc();
         }
-
-        return $coches;
+        return $coches;     
     }
 
     public function plazasDisponibles() {
         $plazas = array();
-
-        $sql = "SELECT Plaza FROM coches";
-        $consulta = $this->conexion->query($sql);
-
-        while ($tupla = $consulta->fetch(PDO::FETCH_ASSOC)) {
+        $sql="select Plaza from coches";
+        $cursor = $this->conexion->query($sql);  
+        while ($tupla = $cursor->fetch_assoc()) {
             array_push($plazas, $tupla['Plaza']);
-        }
-
+        }   
         return $plazas;
     }
 
-    public function guardarVehiculo($datosF) {
-        $Matricula = $datosF['Matricula'];
-        $DNI = $datosF['DNI'];
-        $Marca = $datosF['Marca'];
-        $Modelo = $datosF['Modelo'];
-        $Color = $datosF['Color'];
-        $Plaza = $datosF['Plaza'];
-        $Imagen = $datosF['Imagen'];
+    public function guardarVehiculo($datosF){
+        $Matricula=$datosF['Matricula'];
+        $DNI=$datosF['DNI'];
+        $Marca=$datosF['Marca'];
+        $Modelo=$datosF['Modelo'];
+        $Color=$datosF['Color'];
+        $Plaza=$datosF['Plaza'];
+        $Imagen=$datosF['Imagen'];
 
-        $sql = "INSERT INTO coches (Matricula, DNI, Marca, Modelo, Color, Plaza, Imagen) 
-                VALUES (:matricula, :dni, :marca, :modelo, :color, :plaza, :imagen)";
-
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':matricula', $Matricula);
-        $consulta->bindParam(':dni', $DNI);
-        $consulta->bindParam(':marca', $Marca);
-        $consulta->bindParam(':modelo', $Modelo);
-        $consulta->bindParam(':color', $Color);
-        $consulta->bindParam(':plaza', $Plaza);
-        $consulta->bindParam(':imagen', $Imagen);
-        $exito = $consulta->execute();
-
+        $sql="insert into coches (Matricula,DNI,Marca,Modelo,Color,Plaza,Imagen) VALUES (
+            '$Matricula','$DNI','$Marca','$Modelo','$Color','$Plaza','$Imagen')";
+        
+        $exito=$this->conexion->query($sql);
         return $exito;
     }
 
-    public function modVehiculo($datosF) {
-        $Matricula = $datosF['Matricula'];
-        $Color = $datosF['Color'];
-        $Plaza = $datosF['Plaza'];
-        $Imagen = $datosF['Imagen'];
-
-        if ($Imagen === '') {
-            $sql = "UPDATE coches SET Color = :color, Plaza = :plaza WHERE Matricula = :matricula";
-        } else {
-            $sql = "UPDATE coches SET Color = :color, Plaza = :plaza, Imagen = :imagen WHERE Matricula = :matricula";
+    public function modVehiculo($datosF){
+        $Matricula=$datosF['Matricula'];
+        $Color=$datosF['Color'];
+        $Plaza=$datosF['Plaza'];
+        $Imagen=$datosF["Imagen"];
+        if ($Imagen==='') {
+            $sql="UPDATE coches SET Color = '$Color', Plaza = '$Plaza' WHERE Matricula = '$Matricula'";
+        }else {
+            $sql="UPDATE coches SET Color = '$Color', Plaza = '$Plaza', Imagen = '$Imagen' WHERE Matricula = '$Matricula'";
         }
-
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':color', $Color);
-        $consulta->bindParam(':plaza', $Plaza);
-        $consulta->bindParam(':matricula', $Matricula);
-
-        if ($Imagen !== '') {
-            $consulta->bindParam(':imagen', $Imagen);
-        }
-
-        $exito = $consulta->execute();
-
+        $exito=$this->conexion->query($sql);
         return $exito;
     }
 
-    public function borrarVehiculo($matricula) {
-        $sql = "DELETE FROM coches WHERE Matricula = :matricula";
-
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':matricula', $matricula);
-        $exito = $consulta->execute();
-
+    public function borrarVehiculo($matricula){
+        $sql="delete from coches where Matricula='$matricula'";
+        $exito=$this->conexion->query($sql);
         return $exito;
     }
 
@@ -241,80 +190,57 @@ class ParkingDB {
 
     public function leerClientes() {
         $clientes = array();
-
-        $sql = "SELECT u.*, IFNULL(GROUP_CONCAT(c.Plaza), 'NA') AS Plazas 
+        $sql="SELECT u.*, IFNULL(GROUP_CONCAT(c.Plaza), 'NA') AS Plazas 
                 FROM usuarios u 
                 LEFT JOIN coches c USING (DNI) 
                 GROUP BY u.DNI";
-
-        $consulta = $this->conexion->query($sql);
-
-        while ($tupla = $consulta->fetch(PDO::FETCH_ASSOC)) {
+        $cursor=$this->conexion->query($sql);
+        $tupla = $cursor->fetch_assoc();
+        while ($tupla != null) {
             $cliente = new Cliente($tupla);
             array_push($clientes, $cliente);
+            $tupla = $cursor->fetch_assoc();
         }
-
         return $clientes;
     }
 
-    public function guardarCliente($datosF) {
-        $DNI = $datosF['DNI'];
-        $Nombre = $datosF['Nombre'];
-        $Apellido = $datosF['Apellido'];
-        $Contrasena = $datosF['Contrasena'];
-        $TipoUsu = $datosF['TipoUsu'];
-        $Avatar = $datosF['Avatar'];
-
-        $sql = "INSERT INTO usuarios (DNI, Nombre, Apellido, Contrasena, TipoUsu, Avatar) 
-                VALUES (:dni, :nombre, :apellido, :contrasena, :tipousu, :avatar)";
-
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':dni', $DNI);
-        $consulta->bindParam(':nombre', $Nombre);
-        $consulta->bindParam(':apellido', $Apellido);
-        $consulta->bindParam(':contrasena', $Contrasena);
-        $consulta->bindParam(':tipousu', $TipoUsu);
-        $consulta->bindParam(':avatar', $Avatar);
-        $exito = $consulta->execute();
-
+    public function guardarCliente($datosF){
+        $DNI=$datosF['DNI'];
+        $Nombre=$datosF['Nombre'];
+        $Apellido=$datosF['Apellido'];
+        $Contrasena=$datosF['Contrasena'];
+        $TipoUsu=$datosF['TipoUsu'];
+        $Avatar=$datosF["Avatar"];
+        $sql="insert into usuarios (DNI, Nombre, Apellido, Contrasena, TipoUsu, Avatar) VALUES (
+            '$DNI','$Nombre','$Apellido','$Contrasena','$TipoUsu','$Avatar')";
+        $exito=$this->conexion->query($sql);
         return $exito;
     }
 
-   public function modCliente($datosF) {
-        $DNI = $datosF['DNI'];
-        $Contrasena = $datosF['Contrasena'];
-        $Avatar = $datosF['Avatar'];
-
-        if ($Avatar === '') {
-            $sql = "UPDATE usuarios SET Contrasena = :contrasena WHERE DNI = :dni";
-        } else if ($Contrasena === '') {
-            $sql = "UPDATE usuarios SET Avatar = :avatar WHERE DNI = :dni";
-        } else {
-            $sql = "UPDATE usuarios SET Contrasena = :contrasena, Avatar = :avatar WHERE DNI = :dni";
+    public function modCliente($datosF){
+        $DNI=$datosF['DNI'];
+        $Contrasena=$datosF['Contrasena'];
+        $Avatar=$datosF["Avatar"];
+        if ($Avatar==='') {
+            $sql="UPDATE usuarios SET Contrasena = '$Contrasena' WHERE DNI = '$DNI'";
+        }else if ($Contrasena===''){
+            $sql="UPDATE usuarios SET Avatar = '$Avatar' WHERE DNI = '$DNI'";
+        }else{
+            $sql="UPDATE usuarios SET Contrasena = '$Contrasena', Avatar = '$Avatar' WHERE DNI = '$DNI'";
         }
-
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':dni', $DNI);
-        $consulta->bindParam(':contrasena', $Contrasena);
-        $consulta->bindParam(':avatar', $Avatar);
-        $exito = $consulta->execute();
-
+        $exito=$this->conexion->query($sql);
         return $exito;
     }
 
-    public function borrarCliente($DNI) {
-        $sql = "DELETE FROM usuarios WHERE DNI = :dni";
-
-        $consulta = $this->conexion->prepare($sql);
-        $consulta->bindParam(':dni', $DNI);
-        $exito = $consulta->execute();
-
+    public function borrarCliente($DNI){
+        $sql="delete from usuarios where DNI='$DNI'";
+        $exito=$this->conexion->query($sql);
         return $exito;
     }
     
     /* Cierra la conexión con la base de datos */
     public function cerrar() {
-        $this->conexion = null;
+        $this->conexion->close();
     }
     
 }
