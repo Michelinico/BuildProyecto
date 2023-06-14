aa246ec10f1c808b1be7f44d6e07f0d2c149ebde
Michelinico <116155476+Michelinico@users.noreply.github.com>
2023-06-14 22:53:18 +0200

    Delete build directory

--- build/Modelo/Cabeceras.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: http://parkingmiguel.miguelmarrod.es');
-header('Access-Control-Allow-Credentials: true');
-header("Access-Control-Allow-Headers: content-type");
-header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
-?>
